69a67608000238cdc96145649e7badc3aef42819
Combatd <combatdstar@gmail.com>
2020-03-09 14:57:27 -0700

    Define my_min(integers), my_min_exponential(integers)

--- lib/my_min.rb
+++ lib/my_min.rb
@@ -1,6 +1,13 @@
 # my_min
 # Given a list of integers find the smallest number in the list.
-# O(n) complexity, just keeps adding one iteration compared to a prior number
+# Phase 1
+# O(n^2) time complexity, must run quicksort partitions even if already sorted
+def my_min_exponential(integers)
+    integers.sort!.first
+end
+
+# Phase 2
+# O(n) time complexity, iterates through just once
 def my_min(integers)
     smallest_number = integers.first
     integers.each { |number| smallest_number = number if number < smallest_number }
