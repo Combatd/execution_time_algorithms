edc1ceb7bf2403bbe08fa896635dc25259d5b26f
Combatd <combatdstar@gmail.com>
2020-03-09 18:28:14 -0700

    Define largest_contiguous_subsum(list) Phase 1

--- lib/my_min.rb
+++ lib/my_min.rb
@@ -2,6 +2,10 @@
 # Given a list of integers find the smallest number in the list.
 # Phase 1
 # O(n^2) time complexity, must run quicksort partitions even if already sorted
+# Can be done with nested loops
+# def my_slow_min(list)
+#   list.each { |num| return num if list.all? { |ele| num <= ele } }
+# end
 def my_min_exponential(integers)
     integers.sort!.first
 end
@@ -13,3 +17,38 @@ def my_min(integers)
     integers.each { |number| smallest_number = number if number < smallest_number }
     smallest_number
 end
+
+# You have an array of integers and you want to find the largest 
+# contiguous (together in sequence) sub-sum. 
+# Find the sums of all contiguous sub-arrays and return the max.
+# Phase 1
+# O(n^3ß)
+def largest_contiguous_subsum(integers)
+    sub_sums = []
+    integers.each_with_index do |number, index|
+        list[index..-1].each_with_index do |sub_num, index2|
+            # outer element compares to inner element
+            sub_sums << list[index..index+index2]
+        end
+    end
+
+    # return where the array is greater than previous accumulated sum
+    # inject is a reducer method
+    sub_sums.inject([0]) do |accumulator, array| 
+        if array.sum > accumulator.sum 
+            array
+        else
+            accumulator
+        end
+    end
+end
+
+# Phase 2
+# def largest_contiguous_subsum(integers)
+#     sub_sums = []
+#     integers.each do |ele, idx|
+#         sub_sums << integers.first if idx === 0
+#         sub_sums << integers[-1] if idx === integers.length - 1
+
+#     end
+# end
